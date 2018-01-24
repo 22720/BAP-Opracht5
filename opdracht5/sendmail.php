43766b715817407f532bf02701f0e3ee35e7a2eb
TheSkatingGamer <22720@ma-web.nl>
2018-01-24 14:05:18 +0100

    Add files via upload

---
+++ opdracht5/sendmail.php
@@ -0,0 +1,7 @@
+<?php
+$to ='22720@ma-web.nl';
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$headers = 'From: ' . $_POST['from'];
+mail($to, $subject, $message, $headers);
+echo 'Uw mail is verstuurd';
